3f7b906c180a049e33a72b8faa57eb44c0ce0069
AVSSAM <59338991+AVSSAM@users.noreply.github.com>
2020-11-07 11:15:59 +0530

    Initial api creation

---
+++ API/src/test/java/Group10/example/API/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Group10.example.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
