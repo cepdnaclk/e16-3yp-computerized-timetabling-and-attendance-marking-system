9f00da9efa1c6499c0f5a42bda5e2e8992050f45
NuwanHarsha <nuwan.harshamatrix@gmail.com>
2020-12-25 20:56:55 +0530

    refactored code

--- API/src/main/java/Group10/example/API/Repository/CourseRepository.java
+++ API/src/main/java/Group10/example/API/Repository/CourseRepository.java
@@ -2,17 +2,13 @@ package Group10.example.API.Repository;
 
 import Group10.example.API.Model.Course;
 import Group10.example.API.Model.LectureRoomRef;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
-public interface CourseRepository extends MongoRepository<Course,String> {
+public interface CourseRepository extends MongoRepository<Course, String> {
     Collection<Course> findBySemester(int semester);
 
     Optional<Course> findByCourseNumber(String courseNumber);
