d8b7136364029c25ba0c6e7c7557d3dc60f3605d
AVSSAM <sm201211d@gmail.com>
2020-12-27 11:38:00 +0530

    minior changes to some files

--- API/src/main/java/Group10/example/API/Controller/AdminController.java
+++ API/src/main/java/Group10/example/API/Controller/AdminController.java
@@ -1,10 +1,7 @@
 package Group10.example.API.Controller;
 
-import java.util.ArrayList;
+
 import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,23 +11,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import Group10.example.API.Model.Course;
-import Group10.example.API.Model.Group;
 import Group10.example.API.Model.Lecturer;
 import Group10.example.API.Model.Student;
-import Group10.example.API.Model.groupPayLoad;
 import Group10.example.API.Repository.CourseRepository;
 import Group10.example.API.Repository.LecturerRepository;
 import Group10.example.API.Repository.StudentRepository;
 import Group10.example.API.Service.AdminService;
-import Group10.example.API.Service.GroupService;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
 	
-	@Autowired
-	private GroupService groupService;
+	
 
     @Autowired
     StudentRepository studentRepo;
@@ -50,7 +42,7 @@ public class AdminController {
     	HashMap<String,String> message = new HashMap<String,String>();
     	Student tmpStudent = adminService.insertStudent(student);
     	
-    	if(student != null) message.put("message","Student created successfully" );
+    	if(tmpStudent != null) message.put("message","Student created successfully" );
     	else message.put("message","Student creation failed" );
     	
     	return message;
@@ -110,144 +102,4 @@ public class AdminController {
     	
     }
     	
-
- 
-
-    @PostMapping("/creategroup")
-    public HashMap<String,String> createGroup(@RequestBody Group group){
-        HashMap<String, String> map = new HashMap<>();
-        groupService.saveGroup(group);
-        map.put("msg","successfully created group");
-        map.put("group_id",group.getGroupID());
-        return map;
-    }
-
-    @PostMapping("add/lecturers")
-    public HashMap<String, Object> addLec(@RequestBody groupPayLoad lecturers){
-        HashMap<String, Object> map ;
-        List<String> lecList = lecturers.getIdList();
-        String groupID = lecturers.getGroupId();
-        map  = groupService.addLectures(lecList,groupID);
-        return map;
-
-    }
-
-   @GetMapping(value="/all/lecturers/{group_id}")
-    public List<Lecturer> getLecturers(@PathVariable("group_id") String groupId){
-       Optional<Group> group =groupService.findGroupByID(groupId);
-       List<Lecturer> lecs = new ArrayList<Lecturer>();
-       Set<String> lecID ;
-
-       if(group.isPresent()){
-           lecID = group.get().getLecList();
-           for(String a:lecID){
-
-               Optional<Lecturer> lec = lecRepo.findById(a);
-               lec.ifPresent(c->lecs.add(c));
-
-           }
-
-       }
-
-       return lecs;
-    }
-
-
-    @PostMapping(value="/remove/lecturers")
-    public HashMap<String,Object> removeLec(@RequestBody groupPayLoad lecturers){
-
-        HashMap<String, Object> map;
-        String groupId = lecturers.getGroupId();
-        List<String> lecList = lecturers.getIdList();
-        map = groupService.removeLecFromGroup(lecList,groupId);
-        return map;
-
-
-    }
-
-    @PostMapping("add/students")
-    public HashMap<String, Object> addStudents(@RequestBody groupPayLoad students){
-        HashMap<String, Object> map ;
-        List<String> studentList = students.getIdList();
-        String groupID = students.getGroupId();
-        map  = groupService.addStudents(studentList,groupID);
-        return map;
-
-    }
-
-    @GetMapping(value="/all/students/{group_id}")
-    public List<Student> getStudents(@PathVariable("group_id") String groupId){
-        Optional<Group> group =groupService.findGroupByID(groupId);
-        List<Student> students = new ArrayList<Student>();
-        Set<String> stuID ;
-
-        if(group.isPresent()){
-            stuID = group.get().getStudentList();
-            for(String a:stuID){
-
-                Optional<Student> student = studentRepo.findById(a);
-                student.ifPresent(c->students.add(c));
-
-            }
-
-        }
-
-        return students;
-    }
-
-
-    @PostMapping(value="/remove/students")
-    public HashMap<String,Object> removeStudent(@RequestBody groupPayLoad students){
-
-        HashMap<String, Object> map ;
-        String groupId = students.getGroupId();
-        List<String> studentList = students.getIdList();
-        map = groupService.removeStudentFromGroup(studentList,groupId);
-        return map;
-
-
-    }
-
-    @PostMapping("add/courses")
-    public HashMap<String, Object> addCourse(@RequestBody groupPayLoad course){
-        HashMap<String, Object> map ;
-        List<String> courseList = course.getIdList();
-        String groupID = course.getGroupId();
-        map  = groupService.addCourse(courseList,groupID);
-        return map;
-
-    }
-
-    @GetMapping(value="/all/courses/{group_id}")
-    public List<Course> getCourses(@PathVariable("group_id") String groupId){
-        Optional<Group> group =groupService.findGroupByID(groupId);
-        List<Course> courses = new ArrayList<Course>();
-        Set<String> courseID ;
-
-        if(group.isPresent()){
-           courseID = group.get().getCourseList();
-            for(String a:courseID){
-
-                Optional<Course> course = courseRepo.findById(a);
-                course.ifPresent(c->courses.add(c));
-
-            }
-
-        }
-
-        return courses;
-    }
-
-
-    @PostMapping(value="/remove/courses")
-    public HashMap<String,Object> removeCourse(@RequestBody groupPayLoad course){
-
-        HashMap<String, Object> map ;
-        String groupId = course.getGroupId();
-        List<String> courseList = course.getIdList();
-        map = groupService.removeCourseFromGroup(courseList,groupId);
-        return map;
-
-
-    }
 }
