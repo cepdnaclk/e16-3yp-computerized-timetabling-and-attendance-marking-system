5a31af63a42ca355fa13d52efda29a1c9504ea6b
NuwanHarsha <nuwan.harshamatrix@gmail.com>
2021-01-26 21:05:17 +0530

    lecture room constructor->field injection changed

---
+++ API/src/test/java/Group10/example/API/Controller/LectureRoomControllerTest.java
@@ -0,0 +1,46 @@
+package Group10.example.API.Controller;
+
+import Group10.example.API.Model.LectureRoom;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class LectureRoomControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    TestRestTemplate restTemplate = new TestRestTemplate();
+
+    HttpHeaders headers = new HttpHeaders();
+
+    @Test
+    void addLectureRoom() {
+        LectureRoom lectureRoom = new LectureRoom("test Room",67);
+
+        HttpEntity<LectureRoom> entity = new HttpEntity<LectureRoom>(lectureRoom, headers);
+
+        ResponseEntity<LectureRoom> response = restTemplate.exchange(
+                createURLWithPort("/lecturerooms/add"),
+                HttpMethod.POST, entity, LectureRoom.class);
+
+        String actual = response.getHeaders().get(HttpHeaders.LOCATION).get(0);
+
+        assertTrue(actual.contains("/lecturerooms/add"));
+    }
+
+    private String createURLWithPort(String uri) {
+        return "http://localhost:" + port + uri;
+    }
+}
