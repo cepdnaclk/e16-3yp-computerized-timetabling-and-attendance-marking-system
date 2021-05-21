dedfc602d9b31e47ab1adf0d434aeec8535f339b
NuwanHarsha <nuwan@harshamatrix.com>
2021-05-21 22:46:31 +0530

    apk file added

--- mobileApp/app/src/main/java/com/example/nanocodeams/HomeActivity.java
+++ mobileApp/app/src/main/java/com/example/nanocodeams/HomeActivity.java
@@ -44,15 +44,14 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-                if(item.getItemId() == R.id.tt_des){
-                    startActivity(new Intent(getApplicationContext(),WeekDaysActivity.class));
-                    overridePendingTransition(0,0);
-                    return  true;
-                }
-                else if(item.getItemId() == R.id.noti_des){
-                    startActivity(new Intent(getApplicationContext(),NotificationActivity.class));
-                    overridePendingTransition(0,0);
-                    return  true;
+                if (item.getItemId() == R.id.tt_des) {
+                    startActivity(new Intent(getApplicationContext(), WeekDaysActivity.class));
+                    overridePendingTransition(0, 0);
+                    return true;
+                } else if (item.getItemId() == R.id.noti_des) {
+                    startActivity(new Intent(getApplicationContext(), NotificationActivity.class));
+                    overridePendingTransition(0, 0);
+                    return true;
                 }
                 return false;
             }
@@ -60,47 +59,47 @@ public class HomeActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recycler_view);
 
-        String [] sub = {"CO324","CO322","CO321"};
-        String [] time = {"8-10 am","10-12 am","1-3 pm"};
-        String [] room= {"room 1","room 2","room 3"};
-        String [] course_ID ={"CO321","CO322","CO323","CO324","CO325","EE386"};
-        String [] courses = {"Embedded Systems",
+        String[] sub = {"CO324", "CO322", "CO321"};
+        String[] time = {"8-10 am", "10-12 am", "1-3 pm"};
+        String[] room = {"room 1", "room 2", "room 3"};
+        String[] course_ID = {"CO321", "CO322", "CO323", "CO324", "CO325", "EE386"};
+        String[] courses = {"Embedded Systems",
                 "Data Structures and Algorithms", "Computer Communication Networks II",
-                "Network and Web Application Design","Computer and Network Security"
+                "Network and Web Application Design", "Computer and Network Security"
                 , "Electronics II"
         };
 
         mainModels = new ArrayList<>();
 
-        for (int i=0;i< sub.length;i++){
+        for (int i = 0; i < sub.length; i++) {
 
-            MainModel models = new MainModel(time[i],room[i],sub[i]);
+            MainModel models = new MainModel(time[i], room[i], sub[i]);
             mainModels.add(models);
         }
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(
-                HomeActivity.this,LinearLayoutManager.HORIZONTAL,false);
+                HomeActivity.this, LinearLayoutManager.HORIZONTAL, false);
 
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
 
-        mainAdoptor = new MainAdoptor(mainModels,HomeActivity.this);
+        mainAdoptor = new MainAdoptor(mainModels, HomeActivity.this);
 
         recyclerView.setAdapter(mainAdoptor);
 
         recyclerView_col = findViewById(R.id.recycler_col_view);
         recyclerView_col.setLayoutManager(new LinearLayoutManager(this));
-        listAdaptor = new ListAdaptor(this,course_ID,courses);
+        listAdaptor = new ListAdaptor(this, course_ID, courses);
         recyclerView_col.setAdapter(listAdaptor);
 
 
     }
 
-    public String getToken(){
+    public String getToken() {
 
         String token = null;
 
-        FileInputStream  fileInputStream = null;
+        FileInputStream fileInputStream = null;
         try {
             fileInputStream = openFileInput("token.txt");
             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
@@ -109,8 +108,8 @@ public class HomeActivity extends AppCompatActivity {
             StringBuffer stringBuffer = new StringBuffer();
 
             String lines;
-            while( (lines = bufferedReader.readLine()) != null){
-                stringBuffer.append(lines+"\n");
+            while ((lines = bufferedReader.readLine()) != null) {
+                stringBuffer.append(lines + "\n");
 
             }
             token = stringBuffer.toString();
@@ -123,11 +122,8 @@ public class HomeActivity extends AppCompatActivity {
         }
 
 
-
-
         return token;
     }
-
 
 
 }
--- mobileApp/app/src/main/java/com/example/nanocodeams/LoginActivity.java
+++ mobileApp/app/src/main/java/com/example/nanocodeams/LoginActivity.java
@@ -1,40 +1,24 @@
 package com.example.nanocodeams;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.DownloadManager;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.preference.PreferenceManager;
-import android.telecom.Call;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.android.volley.RequestQueue;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
