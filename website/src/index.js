467fdb977a14526d2cdf46f713be9d29c7b02d04
AVSSAM <sm201211d@gmail.com>
2021-05-21 22:50:50 +0530

    bugs fixed in the website

--- website/src/index.js
+++ website/src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import App from './components/App'
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://efac-attendance.herokuapp.com';
 
 ReactDOM.render(<App />, document.getElementById("root"));
